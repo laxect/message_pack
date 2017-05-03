902223b0d9795fc26af2e568fefc15d6bda8700e
laxect <laxect39@gmail.com>
2017-05-03 22:05:00 +0800

    mp now support memetype key

--- message_pack.py
+++ message_pack.py
@@ -5,12 +5,14 @@ class message(dict):
     # the standard massage class
     def __init__(
             self, recv_from=None, send_to=None, content=None,
-            timestamp=None, user_agent=None, recv_from_name='Unknown'):
+            timestamp=None, user_agent=None,
+            memetype='Text', recv_from_name='Unknown'):
         self['recv_from'] = recv_from
         self['send_to'] = send_to
         self['content'] = content
         self['timestamp'] = timestamp
         self['user_agent'] = user_agent
+        self['memetype'] = memetype
         self['recv_from_name'] = recv_from_name
 
     def recv_from(self):
@@ -32,12 +34,8 @@ class message(dict):
         except json.JSONDecodeError:
             return False
         try:
-            self['recv_from'] = me.get('recv_from')
-            self['send_to'] = me.get('send_to')
-            self['content'] = me.get('content')
-            self['timestamp'] = me.get('timestamp')
-            self['user_agent'] = me.get('user_agent')
-            self['recv_from_name'] = me.get('recv_from_name')
+            for key in me:
+                self[key] = me[key]
             return True
         except KeyError:
             return False
